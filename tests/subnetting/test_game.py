8b664a5a551090fce31e6c2babe704556c95b866
monjiapawne <187591672+monjiapawne@users.noreply.github.com>
2026-08-03 21:26:32 -0400

    tests

--- tests/subnetting/test_game.py
+++ tests/subnetting/test_game.py
@@ -1,19 +1,23 @@
-
-
 from subnetting.client import Response
 
 
 class FakeClient:
-    def __init__(self, get_text: str = "", get_status_code: int = 200):
+    def __init__(self, get_text: str = "", get_status_code: int = 200, post_text: str = "", post_status_code: int = 200):
         self.get_resp = {
             "text": get_text,
             "status_code": get_status_code,
         }
+        self.post_resp = (
+            {
+                "text": post_text,
+                "status_code": post_status_code,
+            },
+        )
 
     def get(self, path: str) -> Response:
         return Response(**self.get_resp)
 
     def post_form(self, path: str, fields: dict | None = None, event_target: str = "") -> Response:
-        return Response()
+        return Response(**self.post_resp)
 
     def close(self) -> None: ...
--- tests/subnetting/test_play.py
+++ tests/subnetting/test_play.py
@@ -4,14 +4,16 @@ from subnetting.game import Game
 
 
 def test_summary():
-    sample = (
+    get_text = (
         '<span id="MainContent_lblSummary" class="bigger">Total Questions: 1<br /> '
         + "Number Correct: 1<br />Number Incorrect: 0<br />TOTAL SCORE: 1<br /></span><br />"
     )
 
-    g = Game(FakeClient(get_text=sample))
+    g = Game(FakeClient(get_text=get_text))
 
-    assert (
-        g.play.summary() ==
-        [("total questions", 1), ("number correct", 1), ("number incorrect", 0), ("total score", 1)]
-    )
+    assert g.play.summary() == [
+        ("total questions", 1),
+        ("number correct", 1),
+        ("number incorrect", 0),
+        ("total score", 1),
+    ]
